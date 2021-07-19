9ee792087307e5b98a131be19a274e60686d4be0
Shivam Kushwah <khushwahashubham0@gmail.com>
2021-07-20 03:43:54 +0530

    merge conflicts

--- src/components/StudentApplication/Professional.js
+++ src/components/StudentApplication/Professional.js
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-import React,{useState}  from 'react'
-import Container from '@material-ui/core/Container'
-import TextField from '@material-ui/core/TextField'
-import {makeStyles} from '@material-ui/core/styles'
-
-import IconButton from '@material-ui/core/IconButton'
-import RemoveIcon from '@material-ui/icons/Remove'
-import AddIcon from '@material-ui/icons/Add'
-
-const useStyles=makeStyles((theme)=>({
-    root:{
-       '& .MuiTextField-root':{
-           margin:theme.spacing(1),
-       }
-    }
-}))
-export default function Professional(props) {
-    const classes=useStyles();
-    
-    return (
-        <Container>
-            <h1 className="my-5">Professional Experience</h1>
-            <form className={classes.root}>
-                {props.inputFields.map((inputField,index)=>(
-                    <div key={index}>
-                        <TextField
-                        name="organization"
-                        label="organization"
-                        value={inputField.organization}
-                        variant="filled"
-                        onChange={event=>props.handleChangeInput(index,event)}>
-
-                        </TextField>
-                        <TextField
-                        name="positionHeld"
-                        label="Position Held"
-                        value={inputField.positionHeld}
-                        variant="filled"
-                        onChange={event=>props.handleChangeInput(index,event)}>
-
-                        </TextField>
-                        <TextField
-                        name="period"
-                        label="Period (From/to)"
-                        value={inputField.period}
-                        variant="filled"
-                        onChange={event=>props.handleChangeInput(index,event)}>
-
-                        </TextField>
-                        <br/>
-                        { (index >0) &&
-                        <IconButton onClick={()=>props.handleRemoveFields(index)}>
-                            <RemoveIcon/>
-                        </IconButton>
-}
-                        <IconButton onClick={()=>props.handleAddFields()}>
-                            <AddIcon/>
-                        </IconButton>
-                    </div>
-                ))}
-            </form>
-            <hr/>
-        </Container>
-    )
-}
-=======
 import React,{useState}  from 'react'
 import Container from '@material-ui/core/Container'
 import TextField from '@material-ui/core/TextField'
@@ -143,4 +76,3 @@ export default function Professional(props) {
         </Container>
     )
 }
->>>>>>> 89950e2d35317388aa99915d40926a7ece163425
